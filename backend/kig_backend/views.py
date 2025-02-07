b67a7097a7bb801c6d63adcd481f28bc56a892d9
frnz1s <franzstevenlabrador@gmail.com>
2025-02-07 18:50:41 +0800

    modified views.py to return Response. Removed redundant functions from views.py.

--- backend/kig_backend/views.py
+++ backend/kig_backend/views.py
@@ -1,32 +1,20 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import ElectricityEmission
 from .serializers import ElectricityEmissionSerializer
 
+# Create your views here.
 class ElectricityEmissionViewSet(viewsets.ModelViewSet):
     queryset = ElectricityEmission.objects.all()
     serializer_class = ElectricityEmissionSerializer
-
-# Create your views here.
-def get_emissions(request):
-    # Create a new entry
-    emission = ElectricityEmission.objects.create(
-        kwh_used=1000
-    )
     
-    # Get CO2 emissions
-    co2_emissions = emission.calculate_emissions()
-    
-    # Get all emissions for display
-    all_emissions = ElectricityEmission.objects.all()
-    total_emissions = sum(e.calculate_emissions() for e in all_emmisions)
-
-
-def record_electricity(request):
-    monthly_kwh = 1000
-    
-    usage = ElectricityEmission.objects.create(
-        kwh_used=monthly_kwh
-    )
-    
-    emissions = usage.calculate_emissions()
+    def list(self, request):
+        emissions = self.get_queryset()
+        serializer = self.get_serializer(emissions, many=True)
+        total_emissions = sum(emission.calculate_emissions() for emission in emissions)
+        
+        return Response({
+            'emissions': serializer.data,
+            'total_emissions': round(total_emissions, 2)
+        })
